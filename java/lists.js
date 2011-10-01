fd3218e2abcc6570c50aa70d75010495f5bdf2bc
MuleSoft <code-contrib@mulesoft.com>
2011-09-30 19:42:04 -0700

    Updating documentation

---
+++ java/lists.js
@@ -0,0 +1,24 @@
+var DATA = [
+      { id:0, label:"history", link:"mule/pubnub.html#history", type:"method" },
+      { id:1, label:"org.mule.module.pubnub", link:"java/org/mule/module/pubnub/package-summary.html", type:"package" },
+      { id:2, label:"org.mule.module.pubnub.MessageListener", link:"java/org/mule/module/pubnub/MessageListener.html", type:"class" },
+      { id:3, label:"org.mule.module.pubnub.PubnubCloudConnector", link:"java/org/mule/module/pubnub/PubnubCloudConnector.html", type:"class" },
+      { id:4, label:"org.mule.module.pubnub.config", link:"java/org/mule/module/pubnub/config/package-summary.html", type:"package" },
+      { id:5, label:"org.mule.module.pubnub.config.HistoryMessageProcessor", link:"java/org/mule/module/pubnub/config/HistoryMessageProcessor.html", type:"class" },
+      { id:6, label:"org.mule.module.pubnub.config.PublishMessageProcessor", link:"java/org/mule/module/pubnub/config/PublishMessageProcessor.html", type:"class" },
+      { id:7, label:"org.mule.module.pubnub.config.PubnubCloudConnectorCapabilitiesAdapter", link:"java/org/mule/module/pubnub/config/PubnubCloudConnectorCapabilitiesAdapter.html", type:"class" },
+      { id:8, label:"org.mule.module.pubnub.config.PubnubCloudConnectorLifecycleAdapter", link:"java/org/mule/module/pubnub/config/PubnubCloudConnectorLifecycleAdapter.html", type:"class" },
+      { id:9, label:"org.mule.module.pubnub.config.RequestMessageProcessor", link:"java/org/mule/module/pubnub/config/RequestMessageProcessor.html", type:"class" },
+      { id:10, label:"org.mule.module.pubnub.config.ServerTimeMessageProcessor", link:"java/org/mule/module/pubnub/config/ServerTimeMessageProcessor.html", type:"class" },
+      { id:11, label:"org.mule.module.pubnub.config.spring", link:"java/org/mule/module/pubnub/config/spring/package-summary.html", type:"package" },
+      { id:12, label:"org.mule.module.pubnub.config.spring.HistoryDefinitionParser", link:"java/org/mule/module/pubnub/config/spring/HistoryDefinitionParser.html", type:"class" },
+      { id:13, label:"org.mule.module.pubnub.config.spring.PublishDefinitionParser", link:"java/org/mule/module/pubnub/config/spring/PublishDefinitionParser.html", type:"class" },
+      { id:14, label:"org.mule.module.pubnub.config.spring.PubnubCloudConnectorConfigDefinitionParser", link:"java/org/mule/module/pubnub/config/spring/PubnubCloudConnectorConfigDefinitionParser.html", type:"class" },
+      { id:15, label:"org.mule.module.pubnub.config.spring.PubnubCloudConnectorNamespaceHandler", link:"java/org/mule/module/pubnub/config/spring/PubnubCloudConnectorNamespaceHandler.html", type:"class" },
+      { id:16, label:"org.mule.module.pubnub.config.spring.RequestDefinitionParser", link:"java/org/mule/module/pubnub/config/spring/RequestDefinitionParser.html", type:"class" },
+      { id:17, label:"org.mule.module.pubnub.config.spring.ServerTimeDefinitionParser", link:"java/org/mule/module/pubnub/config/spring/ServerTimeDefinitionParser.html", type:"class" },
+      { id:18, label:"publish", link:"mule/pubnub.html#publish", type:"method" },
+      { id:19, label:"request", link:"mule/pubnub.html#request", type:"method" },
+      { id:20, label:"server-time", link:"mule/pubnub.html#server-time", type:"method" }
+
+    ];
